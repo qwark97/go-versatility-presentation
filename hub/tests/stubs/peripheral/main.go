5830426debf2f4f9cea86c39432657bd89ccc013
qwark97 <kontakt@marcinplata.pl>
2023-08-27 11:37:41 +0200

    add new endpoint for stubbed peripherals api

--- hub/tests/stubs/peripheral/main.go
+++ hub/tests/stubs/peripheral/main.go
@@ -9,10 +9,14 @@ import (
 func main() {
 	log := slog.Default()
 	http.HandleFunc("/peripheral/temp", func(w http.ResponseWriter, r *http.Request) {
-		log.Info("received request")
+		log.Info("received request: /peripheral/temp")
 		stubbedTemps := []string{"21.5", "22", "21.7"}
 		temp := stubbedTemps[rand.Int()%3]
 		w.Write([]byte(temp))
+	})
+	http.HandleFunc("/peripheral/empty", func(w http.ResponseWriter, r *http.Request) {
+		log.Info("received request: /peripheral/empty")
+		w.Write([]byte(""))
 	})
 	log.Info("starting server")
 	http.ListenAndServe("localhost:9999", nil)

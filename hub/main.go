417ba37272e7e905b93f5e926fa366a5f02995bb
qwark97 <kontakt@marcinplata.pl>
2023-08-29 21:48:08 +0200

    possible usage of the scheduler

--- hub/main.go
+++ hub/main.go
@@ -6,8 +6,9 @@ import (
 	"github.com/qwark97/go-versatility-presentation/hub/flags"
 	"github.com/qwark97/go-versatility-presentation/hub/logger"
 	"github.com/qwark97/go-versatility-presentation/hub/peripherals"
-	"github.com/qwark97/go-versatility-presentation/hub/peripherals/storage"
+	fileStorage "github.com/qwark97/go-versatility-presentation/hub/peripherals/storage"
 	"github.com/qwark97/go-versatility-presentation/hub/scheduler"
+	sqlLiteStorage "github.com/qwark97/go-versatility-presentation/hub/scheduler/storage"
 	"github.com/qwark97/go-versatility-presentation/hub/server"
 )
 
@@ -19,16 +20,11 @@ func main() {
 
 	log := logger.New()
 
-	s := scheduler.New(ctx, log)
-	go func() {
-		if err := s.Start(); err != nil {
-			log.Error("scheduler failed: %v", err)
-			panic(err)
-		}
-	}()
+	sqlLiteS := sqlLiteStorage.New(flagsConf, log)
+	s := scheduler.New(ctx, sqlLiteS, log)
+	fileS := fileStorage.New(flagsConf, log)
+	p := peripherals.New(ctx, s, fileS, log)
 
-	fs := storage.New(flagsConf, log)
-	p := peripherals.New(ctx, s, fs, log)
 	httpServer := server.New(p, flagsConf, log)
 	if err := httpServer.Start(); err != nil {
 		log.Error("server failed: %v", err)

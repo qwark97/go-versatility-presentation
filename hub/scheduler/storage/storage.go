9e99addbf3156165f74230e79c621fec2a37a046
qwark97 <kontakt@marcinplata.pl>
2023-08-31 22:21:41 +0200

    introduce in memory storage

--- hub/scheduler/storage/storage.go
+++ hub/scheduler/storage/storage.go
@@ -1,20 +1,39 @@
 package storage
 
 import (
+	"fmt"
+	"sync"
+
+	"github.com/google/uuid"
 	"github.com/qwark97/go-versatility-presentation/hub/logger"
 )
 
 type Conf interface {
 }
 
-type Storage struct {
+type InMemStorage struct {
 	conf Conf
 	log  logger.Logger
+
+	state *sync.Map
 }
 
-func New(conf Conf, log logger.Logger) Storage {
-	return Storage{
-		conf: conf,
-		log:  log,
+func New(conf Conf, log logger.Logger) *InMemStorage {
+	return &InMemStorage{
+		conf:  conf,
+		log:   log,
+		state: &sync.Map{},
 	}
+}
+
+func (ims *InMemStorage) SaveLastReading(id uuid.UUID, data any) error {
+	ims.state.Store(id, data)
+	return nil
+}
+func (ims *InMemStorage) ReadLastReading(id uuid.UUID) (any, error) {
+	val, present := ims.state.Load(id)
+	if !present {
+		return nil, fmt.Errorf("missing value with id: %s", id)
+	}
+	return val, nil
 }

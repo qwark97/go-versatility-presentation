1d7b0bb68092bb66299693de773a966e678dfbb3
qwark97 <kontakt@marcinplata.pl>
2023-08-31 22:23:18 +0200

    improve data presentation

--- hub/peripherals/storage/model.go
+++ hub/peripherals/storage/model.go
@@ -8,5 +8,20 @@ type Configuration struct {
 	Addr        string    `json:"addr"`
 	Frequency   string    `json:"frequency"`
 	Description string    `json:"description"`
-	Unit        string    `json:"unit"`
+	Unit        Unit      `json:"unit"`
+}
+
+type Unit string
+
+func (u Unit) String() string {
+	if translatedUnit, known := predefinedUnits[u]; known {
+		return translatedUnit
+	} else {
+		return string(u)
+	}
+}
+
+var predefinedUnits = map[Unit]string{
+	"celsius": "°C",
+	"percent": "%",
 }

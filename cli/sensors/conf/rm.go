e172ae24b187862e1267ccdc1e157b721666f158
Marcin Plata <marcin.plata@snowsoftware.com>
2023-09-05 18:25:24 +0200

    implement removing

--- cli/sensors/conf/rm.go
+++ cli/sensors/conf/rm.go
@@ -2,34 +2,37 @@ package conf
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/urfave/cli/v2"
 )
 
 func rm(ctx *cli.Context, presenter Presenter) error {
 	id := ctx.String("id")
-	if !ctx.Bool("all") && id == "" {
+	all := ctx.Bool("all")
+	addr := ctx.String("addr")
+
+	if !all && id == "" {
 		return fmt.Errorf("either -all or -id is required")
 	}
 
 	if id != "" {
-		uri := fmt.Sprintf("http://%s/api/v1/configuration/%s", ctx.String("addr"), ctx.String("id"))
-		return removeConfiguration(uri)
+		uri := fmt.Sprintf("http://%s/api/v1/configuration/%s", addr, id)
+		return apiRequest(http.MethodDelete, uri, nil)
 	} else {
-		uri := fmt.Sprintf("http://%s/api/v1/configurations", ctx.String("addr"))
-		configurations, err := getConfigurations(uri)
+		uri := fmt.Sprintf("http://%s/api/v1/configurations", addr)
+		var configurations []Configuration
+		err := apiRequestWithResponse(http.MethodGet, uri, nil, &configurations)
 		if err != nil {
 			return err
 		}
 		for _, id := range configurations {
-			uri := fmt.Sprintf("http://%s/api/v1/configuration/%s", ctx.String("addr"), id)
-			return removeConfiguration(uri)
+			uri := fmt.Sprintf("http://%s/api/v1/configuration/%s", addr, id.ID)
+			err = apiRequest(http.MethodDelete, uri, nil)
+			if err != nil {
+				return err
+			}
 		}
 	}
-	return nil
-}
-
-func removeConfiguration(uri string) error {
-	fmt.Println("removeConfiguration ID", uri)
 	return nil
 }

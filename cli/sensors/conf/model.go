ce61ffe186857265006ee1a13b44e795a154b55e
Marcin Plata <marcin.plata@snowsoftware.com>
2023-09-05 18:12:35 +0200

    intorduce configuration model

---
+++ cli/sensors/conf/model.go
@@ -0,0 +1,12 @@
+package conf
+
+import "github.com/google/uuid"
+
+type Configuration struct {
+	ID          uuid.UUID `json:"id"`
+	Method      string    `json:"method"`
+	Addr        string    `json:"addr"`
+	Frequency   string    `json:"frequency"`
+	Description string    `json:"description"`
+	Unit        string    `json:"unit"`
+}

1bc63ffe62b679f57d5d231a483cf28c24e8a66b
Marcin Plata <marcin.plata@snowsoftware.com>
2023-09-05 18:12:55 +0200

    add code snippet for simple API request

---
+++ cli/sensors/conf/requests.go
@@ -0,0 +1,38 @@
+package conf
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func apiRequest[T any](method, uri string, requestBody io.Reader, responseContainer *T) error {
+	client := http.DefaultClient
+
+	request, err := http.NewRequest(method, uri, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode%200 >= 100 {
+		return fmt.Errorf("invalid response status code: %s", resp.Status)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, responseContainer)
+	if err != nil {
+		return err
+	}
+	return nil
+}

e1ba273e2ad5a4136b9dfc358209bd8e1ec647d4
qwark97 <kontakt@marcinplata.pl>
2023-09-06 20:30:18 +0200

    add working example of http server on arduino nano33 iot

--- dth/go.mod
+++ dth/go.mod
@@ -1,3 +1,11 @@
 module github.com/qwark97/go-versatility-presentation/dth
 
 go 1.20
+
+require tinygo.org/x/drivers v0.25.0
+
+require (
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- dth/main.go
+++ dth/main.go
@@ -1,5 +1,80 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"machine"
+	"time"
 
+	"tinygo.org/x/drivers/net/http"
+	"tinygo.org/x/drivers/wifinina"
+)
+
+func main() {
+	var led = machine.LED
+	led.Configure(
+		machine.PinConfig{
+			Mode: machine.PinOutput,
+		},
+	)
+
+	spi := machine.NINA_SPI
+	spi.Configure(machine.SPIConfig{
+		Frequency: 8 * 1e6,
+		SDO:       machine.NINA_SDO,
+		SDI:       machine.NINA_SDI,
+		SCK:       machine.NINA_SCK,
+	})
+
+	adaptor := wifinina.New(spi,
+		machine.NINA_CS,
+		machine.NINA_ACK,
+		machine.NINA_GPIO0,
+		machine.NINA_RESETN)
+	adaptor.Configure()
+
+	connectToAP(adaptor)
+	displayIP(adaptor)
+
+	http.UseDriver(adaptor)
+
+	http.HandleFunc("/off", func(w http.ResponseWriter, _ *http.Request) {
+		led.Low()
+		println("off")
+		fmt.Fprintf(w, "led.High()")
+	})
+	http.HandleFunc("/on", func(w http.ResponseWriter, _ *http.Request) {
+		led.High()
+		println("on")
+		fmt.Fprintf(w, "led.High()")
+	})
+
+	http.ListenAndServe(":80", nil)
+}
+
+// connect to access point
+func connectToAP(adaptor *wifinina.Device) {
+	var (
+		ssid = "ssid"
+		pass = "pass"
+	)
+	time.Sleep(2 * time.Second)
+	var err error
+	for i := 0; i < 3; i++ {
+		println("Connecting to " + ssid)
+		err = adaptor.ConnectToAccessPoint(ssid, pass, 10*time.Second)
+		if err == nil {
+			println("Connected.")
+			return
+		}
+	}
+	println(err.Error())
+}
+
+func displayIP(adaptor *wifinina.Device) {
+	ip, _, _, err := adaptor.GetIP()
+	for ; err != nil; ip, _, _, err = adaptor.GetIP() {
+		println(err.Error())
+		time.Sleep(1 * time.Second)
+	}
+	println("IP address: " + ip.String())
 }

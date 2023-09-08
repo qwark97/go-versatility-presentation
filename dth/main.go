f49576d0a1a6d57d3ae959ddde7df60ecf9876dd
qwark97 <kontakt@marcinplata.pl>
2023-09-08 21:31:28 +0200

    implement working example

--- dth/main.go
+++ dth/main.go
@@ -5,17 +5,13 @@ import (
 	"machine"
 	"time"
 
+	"tinygo.org/x/drivers/dht"
 	"tinygo.org/x/drivers/net/http"
 	"tinygo.org/x/drivers/wifinina"
 )
 
 func main() {
-	var led = machine.LED
-	led.Configure(
-		machine.PinConfig{
-			Mode: machine.PinOutput,
-		},
-	)
+	pin := machine.D5
 
 	spi := machine.NINA_SPI
 	spi.Configure(machine.SPIConfig{
@@ -33,48 +29,38 @@ func main() {
 	adaptor.Configure()
 
 	connectToAP(adaptor)
-	displayIP(adaptor)
+
+	device := dht.New(pin, dht.DHT11)
 
 	http.UseDriver(adaptor)
-
-	http.HandleFunc("/off", func(w http.ResponseWriter, _ *http.Request) {
-		led.Low()
-		println("off")
-		fmt.Fprintf(w, "led.High()")
+	http.HandleFunc("/temp", func(w http.ResponseWriter, _ *http.Request) {
+		device.ReadMeasurements()
+		temp, _ := device.Temperature()
+		fmt.Fprintf(w, fmt.Sprintf("%02d.%d", temp/10, temp%10))
 	})
-	http.HandleFunc("/on", func(w http.ResponseWriter, _ *http.Request) {
-		led.High()
-		println("on")
-		fmt.Fprintf(w, "led.High()")
+	http.HandleFunc("/humi", func(w http.ResponseWriter, _ *http.Request) {
+		device.ReadMeasurements()
+		humi, _ := device.Humidity()
+		fmt.Fprintf(w, fmt.Sprintf("%02d.%d", humi/10, humi%10))
 	})
 
 	http.ListenAndServe(":80", nil)
 }
 
-// connect to access point
 func connectToAP(adaptor *wifinina.Device) {
 	var (
 		ssid = "ssid"
 		pass = "pass"
 	)
 	time.Sleep(2 * time.Second)
-	var err error
-	for i := 0; i < 3; i++ {
+	for {
 		println("Connecting to " + ssid)
-		err = adaptor.ConnectToAccessPoint(ssid, pass, 10*time.Second)
-		if err == nil {
-			println("Connected.")
-			return
+		err := adaptor.ConnectToAccessPoint(ssid, pass, 10*time.Second)
+		if err != nil {
+			println("err:", err)
+			continue
 		}
+		println("Connected.")
+		return
 	}
-	println(err.Error())
-}
-
-func displayIP(adaptor *wifinina.Device) {
-	ip, _, _, err := adaptor.GetIP()
-	for ; err != nil; ip, _, _, err = adaptor.GetIP() {
-		println(err.Error())
-		time.Sleep(1 * time.Second)
-	}
-	println("IP address: " + ip.String())
 }

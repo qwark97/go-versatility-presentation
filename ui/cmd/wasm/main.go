cb27c22ce406d1272344a19d2c797bb0083ad126
qwark97 <kontakt@marcinplata.pl>
2023-09-09 15:10:17 +0200

    working test example for wasm based ui

---
+++ ui/cmd/wasm/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"syscall/js"
+)
+
+func main() {
+	fmt.Println("Go Web Assembly")
+	js.Global().Set("readTemperature", readTemperature())
+	select {}
+}
+
+func calculateTemp() (string, error) {
+	temps := []string{"21.3°C", "22°C", "21.6°C"}
+	idx := rand.Int() % 3
+	return string(temps[idx]), nil
+}
+
+func readTemperature() js.Func {
+	jsonfunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 0 {
+			return "Invalid no of arguments passed"
+		}
+		temp, _ := calculateTemp()
+		return temp
+	})
+
+	return jsonfunc
+}
